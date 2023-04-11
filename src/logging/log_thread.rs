e56f6c6a641ca179d098151132f1c2fde3c9b8ea
Will McGloughlin <willem.mcg@gmail.com>
2023-04-10 23:30:34 -0400

    Initial commit

---
+++ src/logging/log_thread.rs
@@ -0,0 +1,55 @@
+use crate::logging::log::Log;
+use std::fs::File;
+use std::io::Write;
+use std::sync::mpsc;
+use std::thread::JoinHandle;
+use std::time::SystemTime;
+use std::{fs, thread};
+
+/// Thread to handle logging of Matricks events
+pub struct LoggingThread {
+    log_path: String,
+}
+
+impl LoggingThread {
+    pub fn new(log_dir: String) -> Self {
+        // create the log directory if it doesn't exist
+        fs::create_dir_all(log_dir.clone()).expect("Unable to make log directory!");
+
+        // get unique time portion of the log file name
+        let time_uid = SystemTime::now()
+            .duration_since(SystemTime::UNIX_EPOCH)
+            .expect("Unable to get duration since epoch!")
+            .as_secs();
+
+        // assemble the path for the log
+        let log_path = format!("{log_dir}/mtx{time_uid}.log");
+
+        Self { log_path }
+    }
+
+    /// Start the logging thread. Returns the join handle and the log sender
+    pub fn start(&mut self) -> (JoinHandle<()>, mpsc::Sender<Log>) {
+        // make the channels
+        let (tx, rx) = mpsc::channel::<Log>();
+
+        // grab the log path to use
+        let log_path = self.log_path.clone();
+
+        // start the thread
+        let handle = thread::spawn(|| Self::log(log_path, rx));
+
+        // return the join handle and the sender
+        (handle, tx)
+    }
+
+    fn log(log_path: String, rx: mpsc::Receiver<Log>) {
+        // create the file
+        let mut file = File::create(log_path).expect("Failed to create log file!");
+
+        // write each log event to the file as the thread recieves them
+        for log in rx {
+            writeln!(&mut file, "{}", log.to_string()).expect("Failed to write log to logfile!");
+        }
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,111 @@
+mod clargs;
+mod logging;
+mod plugin_iterator;
+
+use std::sync::mpsc::Sender;
+use clargs::Args;
+use plugin_iterator::PluginIterator;
+
+use crate::logging::log_thread::LoggingThread;
+use clap::Parser;
+use extism::{Context, Plugin};
+use crate::logging::log::Log;
+use crate::logging::log_origin::LogOrigin;
+use crate::logging::log_type::LogType;
+use crate::plugin_iterator::PluginIteratorError;
+
+const VERSION: Option<&str> = option_env!("CARGO_PKG_VERSION");
+
+fn main() {
+    // parse command line arguments
+    let args = Args::parse();
+
+    // start the logging thread
+    let (log_thread_handle, log_tx) = LoggingThread::new(args.log_dir).start();
+
+    // make an initial version log
+    log_main(
+        &log_tx,
+        LogType::Normal,
+        format!("Starting Matricks v{}", VERSION.unwrap_or("unknown")));
+
+    // make the plugin iterator
+    let plugin_data_list = match PluginIterator::new(args.plugins)
+    {
+        Ok(plugin_iterator) => {plugin_iterator}
+        Err(e) => {
+            log_main(&log_tx, LogType::Error, format!("Failed to instantiate plugin list. See error: <{e:?}>"));
+            log_main(&log_tx, LogType::Normal, format!("Quitting Matricks."));
+            return;
+        }
+    };
+
+    for plugin_result in plugin_data_list {
+        // check if the plugin data was successfully read
+        let (plugin_path, plugin_data) = match plugin_result {
+            Ok(data) => data,
+            Err(error) => {
+                match error {
+                    PluginIteratorError::InvalidSeedPath(path) => {
+                        // the seed path is invalid, meaning that no plugins can be read.
+                        // this should never ever happen
+                        log_main(&log_tx,
+                                 LogType::Error,
+                                 format!("Could not read plugin data due to an invalid or missing path '{path}', resulting in a catastrophic error.")
+                        );
+                        log_main(&log_tx, LogType::Normal, format!("Quitting Matricks."));
+                        return
+                    }
+                    PluginIteratorError::InvalidPluginPath(path) => {
+                        // something went wrong with the path to the active plugin
+                        // we can't run this plugin, but we might be able to run others
+                        log_main(&log_tx,
+                                 LogType::Warning,
+                                 format!("Could not read plugin data due to an invalid or missing path '{path}'. This plugin will be skipped.")
+                        );
+                    }
+                }
+                continue;
+            }
+        };
+
+        // make a new context for the plugin
+        let context = Context::new();
+
+        // make a new instance of the plugin
+        log_main(&log_tx, LogType::Normal, format!("Starting plugin '{plugin_path}'"));
+        let mut plugin = match Plugin::new(&context, plugin_data, [], true) {
+            Ok(plugin) => plugin,
+            Err(_) => {
+                log_main(&log_tx, LogType::Warning, format!("Unable to instantiate plugin '{plugin_path}'. This plugin will be skipped."));
+                continue;
+            }
+        };
+
+        // call setup and update here
+
+    }
+
+
+    // close channels
+    log_main(&log_tx, LogType::Normal, format!("Closing thread channels..."));
+    drop(log_tx);
+
+    // join logging and matrix control threads
+    log_main(&log_tx, LogType::Normal, format!("Joining threads..."));
+    log_thread_handle
+        .join()
+        .expect("Unable to join log thread!");
+
+    log_main(&log_tx, LogType::Normal, format!("Quitting Matricks."));
+}
+
+/// Send a log from the main thread
+fn log_main(log_tx: &Sender<Log>, log_type: LogType, description: String) {
+    log_tx.send(
+        Log::new(
+            LogOrigin::MainThread,
+            log_type,
+            description
+        )).expect("Unable to send log from main thread!");
+}
