0c476e8df37cae1041a11b61fd8814c4571df39d
Will McGloughlin <willem.mcg@gmail.com>
2023-08-08 21:46:21 -0500

    Add Rust example

---
+++ examples/example_rust/src/lib.rs
@@ -0,0 +1,63 @@
+use extism_pdk::*;
+use lazy_static::lazy_static;
+use matricks_plugin::{MatrixConfiguration, PluginUpdate};
+use serde_json::from_str;
+use std::ops::DerefMut;
+use std::sync::{Arc, Mutex};
+
+lazy_static! {
+    static ref CONFIG: Arc<Mutex<MatrixConfiguration>> =
+        Arc::new(Mutex::new(MatrixConfiguration::default()));
+    static ref COUNTER: Arc<Mutex<u8>> = Arc::new(Mutex::new(0));
+}
+
+#[plugin_fn]
+pub fn setup(cfg_json: String) -> FnResult<()> {
+    // Set the matrix configuration struct
+    let mut config = CONFIG.lock().unwrap();
+    let config = config.deref_mut();
+    *config = from_str(&*cfg_json)
+        .expect("Unable to deserialize matrix config!");
+
+    Ok(())
+}
+
+#[plugin_fn]
+pub fn update(_: ()) -> FnResult<Json<PluginUpdate>> {
+    // Get the static config object
+    let config = CONFIG.lock().unwrap();
+
+    // Get the static counter
+    let mut counter = COUNTER.lock().unwrap();
+    let counter = counter.deref_mut();
+
+    // Make a 2D vector of all red, with intensity set by the counter value
+    let mut led_state: Vec<Vec<[u8; 4]>> = vec![];
+    for y in 0..config.width {
+        led_state.push(vec![]);
+        for _x in 0..config.height {
+            led_state[y].push([*counter, *counter, *counter, 0]); // BGRA order!
+        }
+    }
+
+    // Increment counter and determine whether to stop providing updates
+    if *counter == 255 {
+        // If the counter is 255, let's stop the plugin and log why
+        Ok(Json(PluginUpdate {
+            state: led_state,
+            done: true,
+            log_message: Some(vec!["Done fading to white!".to_string()]),
+            ..Default::default()
+        }))
+    } else {
+        // If the counter is less than 255, increment the counter
+        *counter += 1;
+
+        Ok(Json(PluginUpdate {
+            state: led_state,
+            done: false,
+            ..Default::default()
+        }))
+    }
+
+}
